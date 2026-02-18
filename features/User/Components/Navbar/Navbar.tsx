5390e9af25bee6625561821cb2b29a4a0d1ee718
Vincent Dwi Hartanto <vincentkartala@gmail.com>
2026-02-18 11:29:00 +0700

    fix: hero section

--- features/User/Components/Navbar/Navbar.tsx
+++ features/User/Components/Navbar/Navbar.tsx
@@ -11,17 +11,32 @@ const Navbar = () => {
                 <div className="bg-white p-2 px-4 rounded-[80px]">
                     <Image src={"/logo.webp"} alt={""} width={55} height={55}></Image>
                 </div>
-                <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex gap-5 w-auto">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-5 w-auto">
                     {NAVBAR_LINKS.map((link) => (
-                        <a key={link.href} href={link.href} className="px-4 py-2 rounded-[80px] text-white flex gap-1 items-center">{link.label}{link.icon}</a>
+                    <a
+                        key={link.href}
+                        href={link.href}
+                        className="relative isolate px-4 py-2 rounded-[80px] text-white flex gap-1 items-center overflow-hidden
+                                after:content-[''] after:absolute after:inset-0
+                                after:bg-white after:origin-center after:scale-x-0
+                                after:transition-transform after:duration-400
+                                after:z-0
+                                hover:after:scale-x-100 hover:text-black duration-500 transition-color"
+                    >
+                        <span className="relative z-10 flex gap-1 items-center">
+                        {link.label}
+                        {link.icon}
+                        </span>
+                    </a>
                     ))}
                 </div>
-                <div className="group bg-white rounded-[80px] px-4 py-2 flex gap-2 items-center relative overflow-hidden border-white border-1 cursor-pointer">
+
+                <div className="group bg-transparent text-white rounded-[80px] px-4 py-2 flex gap-2 items-center relative overflow-hidden border-white border-1 cursor-pointer">
                     
                     {/* background animasi */}
                     <span className="
                         absolute inset-0 
-                        bg-black 
+                        bg-white 
                         scale-x-0 
                         group-hover:scale-x-100 
                         transition-transform 
@@ -32,8 +47,8 @@ const Navbar = () => {
                     "></span>
 
                     {/* content */}
-                    <FaPhoneAlt className="relative z-10 text-black group-hover:text-white transition-all duration-500 ease" />
-                    <button className="relative z-10 rounded-[80px] text-black group-hover:text-white transition-all duration-500 ease">
+                    <FaPhoneAlt className="relative z-10 text-white group-hover:text-black transition-all duration-500 ease" />
+                    <button className="relative z-10 rounded-[80px] text-white group-hover:text-black transition-all duration-500 ease">
                         Contact Us
                     </button>
                 </div>
--- features/User/landing-page/Components/Hero.tsx
+++ features/User/landing-page/Components/Hero.tsx
@@ -2,6 +2,8 @@ import FloatingLines from "@/components/FloatingLines";
 import BlurText from "@/components/BlurText"
 import ShinyText from "@/components/ShinyText"
 import { Glassmorph } from "@/lib/constant/Glassmorph";
+import "@/public/animation/animation.css"
+import { ArrowRight } from "lucide-react";
 
 const Hero = () => {
     return (
@@ -12,26 +14,35 @@ const Hero = () => {
                 lineDistance={[10]}
                 bendRadius={10}
                 bendStrength={-0.5}
-                interactive={true}
-                parallax={true} 
+                interactive={false}
+                parallax={false} 
                 linesGradient={undefined}
                 topWavePosition={undefined}
                 middleWavePosition={undefined}
                 animationSpeed={2}
             />
-            <div className="max-w-[1400px] m-auto flex justify-start items-end h-full py-10 px-10 xl:px-0">
+            <div className="max-w-[1400px] m-auto flex justify-start items-end h-full py-10 px-10 xl:px-0 relative">
                 <div className="flex flex-col text-white gap-2">
-                    <BlurText
-                        text="PT Tidex Titan Persada"
-                        delay={200}
-                        animateBy="words"
-                        direction="top"
-                        className="text-3xl font-semibold"
-                        animationFrom={undefined} 
-                        animationTo={undefined} 
-                        onAnimationComplete={undefined}/>
+                    <div className={`absolute right-20 top-50 w-64 rounded-xl ${Glassmorph} p-6`}>
+                        <div className="text-4xl font-bold text-white">25+</div>
+                        <div className="mt-1 text-md text-white font-semibold">
+                        Years of Experience
+                        </div>
+                        <p className="mt-3 text-sm text-white font-light">
+                        Since 1997, delivering integrated <span className="text-white">IT</span>, <span className="text-white">ICT</span>, and <span className="text-white">IoT</span> solutions.
+                        </p>
+                    </div>
+
+                    <div className={`absolute right-72 top-110 w-64 rounded-xl ${Glassmorph} p-6`}>
+                        <div className="text-4xl font-bold text-white">50+</div>
+                        <div className="mt-1 text-md text-white font-semibold">Big Projects</div>
+                        <p className="mt-3 text-sm text-white font-light">
+                        Successfully delivering large-scale projects <span className="text-white">across industries.</span>
+                        </p>
+                    </div>
+
                     <ShinyText
-                        text="Trusted to Commitment"       
+                        text="Trusted to Commitment"
                         speed={2}
                         delay={1}
                         color="#b5b5b5"
@@ -41,12 +52,30 @@ const Hero = () => {
                         yoyo={false}
                         pauseOnHover={false}  
                         disabled={false}
-                        className="text-8xl font-bold max-w-2xl" 
+                        className="text-3xl font-bold" 
                     />
-                    <p className="max-w-2xl text-gray-400">We believe the values of commitment, willingness to learn and develop and make customer-centric, are the values and culture we build in teams that will bring the company to <span className="text-white">grow</span>, <span className="text-white">strong</span> and <span className="text-white">healthy</span>.</p>
+                    <BlurText
+                        text="PT Tidex Titan Persada"    
+                        delay={200}
+                        animateBy="words"
+                        direction="top"
+                        className="text-8xl font-bold max-w-3xl"
+                        animationFrom={undefined} 
+                        animationTo={undefined} 
+                        onAnimationComplete={undefined}/>
+                    <p className="max-w-2xl text-white">We believe the values of commitment, willingness to learn and develop and make customer-centric, are the values and culture we build in teams that will bring the company to <span className="text-white">grow</span>, <span className="text-white">strong</span> and <span className="text-white">healthy</span>.</p>
                     <div className="flex gap-3 mt-3">
-                        <button className="bg-white text-black py-2 px-4 rounded-lg">Our product</button>
-                        <button className={`text-white py-2 px-4 rounded-lg ${Glassmorph}`}>Lets Talk</button>
+                        <button className="bg-white text-black py-2 px-4 rounded-lg cursor-pointer">Our product</button>
+                        <button
+                            className={`text-white py-2 px-4 rounded-lg cursor-pointer flex gap-1 items-center group ${Glassmorph}`}
+                        >
+                        Lets Talk
+                        <ArrowRight
+                            size={15}
+                            className="transition-transform duration-300 group-hover:translate-x-1"
+                        />
+                        </button>
+
                     </div>
                 </div>
             </div>
